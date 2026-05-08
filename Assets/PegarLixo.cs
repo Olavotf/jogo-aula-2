314045665703f028a9694667df9c5a935729a108
José <olavotf3@gmail.com>
2026-05-08 16:09:41 -0300

    audio2

--- Assets/PegarLixo.cs
+++ Assets/PegarLixo.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PegarLixo : MonoBehaviour
 {
+    public AudioSource source;
     public LixoSpawnerController lixoSpawnerController;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Lixo"))
         {
             Destroy(collision.gameObject);
+            source.Play();
             lixoSpawnerController.AddToPoints(1);
         }
     }
